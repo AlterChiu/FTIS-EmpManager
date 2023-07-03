44fc55ce23925b34c0d8dffaf11a5bf1cfcdd05f
brianlin12345 <brianlin12345@taiglobal.com.tw>
2023-07-03 14:30:23 +0800

    個人資料 頁簽多筆(員工資料、基本資料、學歷)

---
+++ Controllers/Emp/EmpDa1Controller.cs
@@ -0,0 +1,23 @@
+﻿using Dou.Models.DB;
+using FtisHelperV2.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DouImp.Controllers.Emp
+{
+    public class EmpDa1Controller : Dou.Controllers.AGenericModelController<F22cmmEmpData>
+    {
+        // GET: EmpDa1 基本資料
+        public ActionResult Index()
+        {
+            return View();
+        }
+        protected override IModelEntity<F22cmmEmpData> GetModelEntity()
+        {
+            return new Dou.Models.DB.ModelEntity<F22cmmEmpData>(new DouImp.Models.DouModelContextExt());
+        }
+    }
+}
---
+++ Controllers/Emp/EmpDa4Controller.cs
@@ -0,0 +1,23 @@
+﻿using Dou.Models.DB;
+using FtisHelperV2.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DouImp.Controllers.Emp
+{
+    public class EmpDa4Controller : Dou.Controllers.AGenericModelController<F22cmmEmpDa4>
+    {
+        // GET: EmpDa4 學歷
+        public ActionResult Index()
+        {
+            return View();
+        }
+        protected override IModelEntity<F22cmmEmpDa4> GetModelEntity()
+        {
+            return new Dou.Models.DB.ModelEntity<F22cmmEmpDa4>(new DouImp.Models.DouModelContextExt());
+        }
+    }
+}
